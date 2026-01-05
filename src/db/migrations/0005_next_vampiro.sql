9ec4dd3d3533ea9f902a88fee115901796b174b7
Philip Zhan <h6zhan@gmail.com>
2026-01-04 20:28:31 -0800

    fix migration

--- src/db/migrations/0005_next_vampiro.sql
+++ src/db/migrations/0005_next_vampiro.sql
@@ -29,7 +29,7 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "messagev2" ADD CONSTRAINT "messagev2_chat_id_chat_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chat"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "messagev2" ADD CONSTRAINT "messagev2_chat_id_chat_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chatv2"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- src/db/migrations/0006_third_bill_hollister.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "messagev2" DROP CONSTRAINT "messagev2_chat_id_chat_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "messagev2" ADD CONSTRAINT "messagev2_chat_id_chatv2_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chatv2"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
