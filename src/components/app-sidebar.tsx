a9a7c2b94a9fd40bf3a28d8c475a73ccf8f1d951
Philip Zhan <h6zhan@gmail.com>
2026-01-05 20:07:10 -0800

    new chat button

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -110,7 +110,7 @@ export function AppSidebar({ user }: { user: Session | undefined }) {
                       className="h-8 p-1 md:h-fit md:p-2"
                       onClick={() => {
                         setOpenMobile(false);
-                        router.push("/");
+                        router.push("/new");
                         router.refresh();
                       }}
                       type="button"
